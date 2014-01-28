b6453a77544a0dba3b35321e068003d15b5a2889
hylasoft <g.tagliabue@hylasoftusa.com>
2014-01-28 13:29:39 -0600

    [BreadApi] Add Ninject IoC

--- BreadApi/BreadApi/Controllers/ValuesController.cs
+++ BreadApi/BreadApi/Controllers/ValuesController.cs
@@ -3,33 +3,38 @@ using System.Web.Http;
 
 namespace BreadApi.Controllers
 {
-    public class ValuesController : ApiController
+  public class ValuesController : ApiController
+  {
+    public ValuesController(IList<int> l)
     {
-        // GET api/values
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        public void Delete(int id)
-        {
-        }
+      l.Add(1);
     }
+
+    // GET api/values
+    public IEnumerable<string> Get()
+    {
+      return new string[] { "value1", "value2" };
+    }
+
+    // GET api/values/5
+    public string Get(int id)
+    {
+      return "value";
+    }
+
+    // POST api/values
+    public void Post([FromBody]string value)
+    {
+    }
+
+    // PUT api/values/5
+    public void Put(int id, [FromBody]string value)
+    {
+    }
+
+    // DELETE api/values/5
+    public void Delete(int id)
+    {
+    }
+  }
 }
