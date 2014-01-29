34926615980591dfb8842d9e8ad64816d7b0801c
itajaja <giacomo.tag@gmail.com>
2014-01-29 15:39:23 -0600

    Change Layout and index view

--- BreadApi/BreadApi/Controllers/HomeController.cs
+++ BreadApi/BreadApi/Controllers/HomeController.cs
@@ -1,12 +1,28 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Hylasoft.BreadApi.Services;
+using Hylasoft.BreadEngine;
+using m = Hylasoft.BreadApi.Models;
 
 namespace Hylasoft.BreadApi.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    private readonly List<BreadLoader> _breads = BreadService.Get();
+
+    public ActionResult Index()
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
+      var model = new m.IndexModel{Breads = _breads.Select(b => b.Name).ToList()};
+      return View(model);
     }
+
+    public ActionResult Bread(string id)
+    {
+      var breadInstance = _breads.Single(b => String.Equals(b.Name, id, StringComparison.CurrentCultureIgnoreCase));
+      var model = new m.BreadModel { Bread = new m.BreadLoader(breadInstance)};
+      return View(model);
+    }
+  }
 }
