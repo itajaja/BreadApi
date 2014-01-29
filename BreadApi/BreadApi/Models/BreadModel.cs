01ab54ad231e7daf03e16de9b4890f67c7f53973
itajaja <giacomo.tag@gmail.com>
2014-01-29 15:38:44 -0600

    Add BreadPage and corresponding model
    Also added model for Index Page

---
+++ BreadApi/BreadApi/Models/BreadModel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace Hylasoft.BreadApi.Models
+{
+  public class BreadModel
+  {
+    public BreadLoader Bread { get; set; }
+  }
+
+  public class BreadLoader
+  {
+    public BreadLoader(BreadEngine.BreadLoader b)
+    {
+      Name = b.Name;
+      Breads = new List<Bread>();
+      foreach (var bread in b.Breads)
+      {
+        var br = new Bread
+        {
+          Name = bread.Name,
+          Methods = bread.Methods,
+          Entity = bread.EntityType == null ? "" : bread.EntityType.Name
+        };
+        Breads.Add(br);
+      }
+    }
+
+    public string Name { get; set; }
+    public IList<Bread> Breads { get; set; }
+  }
+
+  public class Bread
+  {
+    public string Name { get; set; }
+    public IList<string> Methods { get; set; }
+    public string Entity { get; set; }
+  }
+}
---
+++ BreadApi/BreadApi/Models/IndexModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Hylasoft.BreadApi.Models
+{
+  public class IndexModel
+  {
+    public IList<string> Breads { get; set; }
+  }
+}
