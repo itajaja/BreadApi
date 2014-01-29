f9271a4fe22bd76bce023f48f9e5004b27a4f202
itajaja <giacomo.tag@gmail.com>
2014-01-29 13:49:32 -0600

    Add BreadService

---
+++ BreadApi/BreadApi/Services/BreadService.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Hylasoft.BreadEngine;
+
+namespace Hylasoft.BreadApi.Services
+{
+  public static class BreadService
+  {
+    private static List<BreadLoader> _breads;
+
+    private static void InitializeBreads()
+    {
+      _breads = new List<BreadLoader>();
+      foreach (var file in Config.Val(Config.Keys.BreadDlls).Split(','))
+      {
+        var path = Path.Combine(Config.Val(Config.Keys.PathToSit), file);
+        _breads.Add(new BreadLoader(path));
+      }
+    }
+
+    /// <summary>
+    /// Gets the list of available breads. Loads the dll only the first time it gets called
+    /// </summary>
+    /// <returns>The list of available breads</returns>
+    public static List<BreadLoader> Get()
+    {
+      if (_breads == null)
+        InitializeBreads();  
+      return _breads;
+    }
+  }
+}
