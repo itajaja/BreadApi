69f782f3f5bd8a10765edda8109fc0a8f0b8869c
itajaja <giacomo.tag@gmail.com>
2014-02-18 14:56:34 -0600

    Reimplement Bread Page with Angular
    1st half

--- BreadApi/BreadApi/angular/controllers.js
+++ BreadApi/BreadApi/angular/controllers.js
@@ -3,10 +3,30 @@
 /* Controllers */
 
 angular.module('breadApp.controllers', []).
-  controller('breadController', function ($scope) {
+controller('breadController', function ($scope, breadApi) {
 
-    $scope.alert = function (text) {
-      alert(text);
-    };
+  $scope.loading = true;
 
-  });
+  //todo POM should be a parameter
+  $scope.getBreads = function (bread) {
+    $scope.bread = bread;
+    breadApi.breads($scope.bread).success(function (data) {
+      $scope.breads = data;
+      $scope.loading = false;
+    });
+  };
+
+  $scope.getMethods = function (breadClass) {
+    $scope.loading = true;
+    $scope.methods = [];
+    $scope.selectedBread = breadClass;
+    breadApi.methods($scope.bread, breadClass.Name).success(function (data) {
+      $scope.methods = data;
+      $scope.loading = false;
+    });
+  };
+    
+  $scope.isSelectedBread = function (bread) {
+    return bread == $scope.selectedBread;
+  };
+});
--- BreadApi/BreadApi/angular/services.js
+++ BreadApi/BreadApi/angular/services.js
@@ -3,17 +3,18 @@
 /* Services */
 angular.module('breadApp.services', []).
 value('hostUrl', 'http://localhost:55181/').
-service('breadApi', function ($http, hostUrl) {
+value('breadApiUrl', 'http://localhost:55181/breads/').
+service('breadApi', function ($http, breadApiUrl) {
   this.invoke = function (bread, breadClass, method) {
-    return $http.post(hostUrl + bread + '/' + breadClass + '/' + method);
+    return $http.post(breadApiUrl + bread + '/' + breadClass + '/' + method);
   };
   this.params = function (bread, breadClass, method) {
-    return $http.post(hostUrl + bread + '/' + breadClass + '/' + method + '/params');
+    return $http.post(breadApiUrl + bread + '/' + breadClass + '/' + method + '/params');
   };
   this.breads = function (bread) {
-    return $http.post(hostUrl + bread);
+    return $http.post(breadApiUrl + bread);
   };
   this.methods = function (bread, breadClass) {
-    return $http.post(hostUrl + bread + '/' + breadClass);
+    return $http.post(breadApiUrl + bread + '/' + breadClass);
   };
 });
